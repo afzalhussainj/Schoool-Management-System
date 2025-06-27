d5fa30fde89ec2119826ee229151be9149c34a11
afzalhussainj <afzalhussainjatt@gmail.com>
2025-06-27 18:53:55 +0500

    updated views

--- scholio/schools/serializers.py
+++ scholio/schools/serializers.py
@@ -1,7 +1,7 @@
 from . models import *
 from rest_framework import serializers
 
-class SchoolsSerializer(serializers.ModelSerializer):
+class SchoolSerializer(serializers.ModelSerializer):
     class Meta:
         model = School
         fields = '__all__'
--- scholio/users/views.py
+++ scholio/users/views.py
@@ -2,7 +2,7 @@
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework import generics, permissions, status
-from schools.serializers import SchoolsSerializer
+from schools.serializers import SchoolSerializer
 from schools.models import School
 from .models import Principal
 from .serializers import *
@@ -12,17 +12,28 @@ from .permissions import IsBranchManager, IsSchoolOwner
 
 # Create your views here.
 
-class SchoolCreateAPIview(APIView):
+class SchoolAPIview(APIView):
     # queryset = School.objects.all()
     permission_classes = [permissions.IsAdminUser]
     authentication_classes = [JWTAuthentication]
 
     def post(self, request):
-        serializer = SchoolsSerializer(data=request.data)
+        serializer = SchoolSerializer(data=request.data)
         if serializer.is_valid():
             return JsonFormatedResponse(data=serializer.data,message='',status=status.HTTP_201_CREATED)
         
-class BranchManagerCreateAPIview(APIView):
+    def put(self, request, pk, format=None):
+        try:
+            school = self.get_object(pk)
+        except School.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = SchoolSerializer(school, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BranchManagerAPIview(APIView):
     # queryset = School.objects.all()
     permission_classes = [permissions.IsAdminUser, IsSchoolOwner]
     authentication_classes = [JWTAuthentication]
@@ -32,8 +43,17 @@ class BranchManagerCreateAPIview(APIView):
         if serializer.is_valid():
             return JsonFormatedResponse(data=serializer.data,message='',status=status.HTTP_201_CREATED)
 
-
-
+    def put(self, request, pk, format=None):
+        try:
+            school = self.get_object(pk)
+        except School.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = SchoolSerializer(school, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 class PrincipalCreateAPIview(APIView):
     # queryset = Principal.objects.all()
     # serializer_class = PrincipalSerializer
