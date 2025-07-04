e8de823a1bb86ee5c112e2fb12793d7b29ebb1ca
afzalhussainj <afzalhussainjatt@gmail.com>
2025-07-04 18:10:29 +0500

    schoolowner url fixed

--- scholio/users/urls.py
+++ scholio/users/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('branchmanager/', views.BranchManagerAPIview.as_view(), name='BranchManager' ),
     path('branchmanager/<int:pk>', views.BranchManagerpkAPIview.as_view(), name='BranchManager' ),
-    path('owner/', views.OwnerAPIview.as_view(), name='Owner' ),
-    path('owner/<int:pk>', views.OwnerpkAPIview.as_view(), name='Owner' ),
+    path('schoolowner/', views.OwnerAPIview.as_view(), name='Owner' ),
+    path('schoolowner/<int:pk>', views.OwnerpkAPIview.as_view(), name='Owner' ),
     path('principal/', views.PrincipalAPIview.as_view(), name='Principal' ),
     path('principal/<int:pk>', views.PrincipalpkAPIview.as_view(), name='Principal' ),
 
